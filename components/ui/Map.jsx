e89d2796496ac1f6fc7717af2098f974dd0eed9c
Karel Rodríguez Varona <kwaref@gmail.com>
2022-06-22 12:13:25 -0400

    fix: permite modificar la altura del mapa de la pagina contacto mediante el paso de una propiedad. Default to 520

--- components/ui/Map.jsx
+++ components/ui/Map.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Map = ({ height }) => {
+export const Map = ({ height=520 }) => {
 	return (
 		<iframe
 			src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3784.610144710343!2d-66.06765988513654!3d18.456001887446597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c036f46663f7843%3A0x723eb05ffbe83d92!2sAshford%20Hospital!5e0!3m2!1ses-419!2scu!4v1655669432059!5m2!1ses-419!2scu'
